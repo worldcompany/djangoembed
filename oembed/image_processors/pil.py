2b5c896d0289af21ff892b2e3c93c7c62c63f75d
Charles Leifer <coleifer@gmail.com>
2011-04-21 16:16:15 -0500

    Rewriting the PIL resizer to play more nicely with django's various storage backends

--- oembed/image_processors/pil.py
+++ oembed/image_processors/pil.py
@@ -1,16 +1,41 @@
 import Image
 import os
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from django.core.files import File
+from django.core.files.storage import default_storage
+
 
 class PIL_Resizer(object):
     def resize(self, image_field, new_width, new_height):
-        img_file, img_ext = os.path.splitext(image_field.path)
         img_url, img_ext = os.path.splitext(image_field.url)
+        img_path, img_ext = os.path.splitext(image_field.name)
         
         append = '_%sx%s%s' % (new_width, new_height, img_ext)
+
         new_url = img_url + append
-        if not os.path.isfile(img_file + append):
+        new_path = img_path + append
+
+        if not default_storage.exists(new_path):
+            # load a file-like object at the image path
+            source_file = default_storage.open(img_path)
+
+            # load up the image using PIL and retrieve format
             img = Image.open(image_field.path)
+            format = img.format
+            
+            # perform resize
             img = img.resize((new_width, new_height), Image.ANTIALIAS)
-            img.save(img_file + append)
+
+            # create a file-like object to store resized data
+            img_buffer = StringIO()
+            img.MAXBLOCK = 1024*1024
+            img.save(img_buffer, format=format)
+
+            # save data
+            default_storage.save(new_path, File(img_buffer))
         
         return (new_url, new_width, new_height)
