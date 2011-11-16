97c95522b7edc8ed223955ca8907ed791177f136
Charles Leifer <coleifer@gmail.com>
2011-11-16 09:48:39 -0600

    Revert "Removing mock_request() and getting the request from the Context instead, this way all other request context should be included. Fix for Issue #13"
    This reverts commit e56be6bd16226b7553c2779764d0c7728c0d38af.

--- oembed/parsers/base.py
+++ oembed/parsers/base.py
@@ -21,7 +21,7 @@ class BaseParser(object):
         - original_url: the url that was passed to the consumer
         """
         provided_context = context or Context()
-        context = RequestContext(provided_context["request"])
+        context = RequestContext(mock_request())
         context.update(provided_context)
         
         # templates are named for the resources they display, i.e. video.html
