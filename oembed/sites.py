4e562421cb998b483676cc4d0b77756393b97e8a
Charles Leifer <coleifer@gmail.com>
2012-04-20 15:25:51 -0500

    Small fixes

--- oembed/sites.py
+++ oembed/sites.py
@@ -179,7 +179,7 @@ class ProviderSite(object):
         Load up StoredProviders from url if it is an oembed scheme
         """
         headers, response = fetch_url(url)
-        if headers['content-type'] in ('application/json', 'text/javascript'):
+        if headers['content-type'].split(';')[0] in ('application/json', 'text/javascript'):
             provider_data = json.loads(response)
             return self.store_providers(provider_data)
     
